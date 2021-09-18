794035126d324f675ec65c295e99f19d2f88f64e
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-09-18 18:40:55 -0400

    new mp3 and chords music21

--- music.py
+++ music.py
@@ -1,5 +1,15 @@
-from spleeter import separator
-from spleeter.separator import Separator
+from music21 import *
 
-separator = Separator('spleeter:2stems')
-separator.separate_to_file('audio.mp3', 'output')
+corpus.addPath('.')
+allBach = corpus.parse('./Dead_Half_Way.mxl')
+
+partStream = allBach.parts.stream()
+
+for n in allBach.flat.notes:
+    print(n)
+    if type(n) is note.Note:
+        print(f"Note: {n.pitch.name} {n.pitch.octave} {n.duration.quarterLength}")
+    else:
+        for c in n.pitches:
+            print(f"Note: {c.name} {c.octave}")
+        print(n.duration.quarterLength)
