7b52a35fb2fd4dedce0fb834d1b7e261ff7d0941
Elias Hawa <elias.hawa2468@gmail.com>
2021-09-18 14:41:40 -0400

    cnfwebewfew

--- music.py
+++ music.py
@@ -2,4 +2,4 @@ from spleeter import separator
 from spleeter.separator import Separator
 
 separator = Separator('spleeter:2stems')
-separator.separate_to_file()
+separator.separate_to_file('audio.mp3', 'output')
