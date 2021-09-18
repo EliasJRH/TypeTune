03b83c9ee08e976d868e2128936906241facfe31
Elias Hawa <elias.hawa2468@gmail.com>
2021-09-18 19:03:58 -0400

    Add bad post request

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+const {spawn} = require('child_process')
 const session = require('express-session')
 const pug = require('pug')
 
@@ -6,7 +8,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('home.html', {root: __dirname })
+})
+
+app.post('/song', (req, res) =>{
+  console.log(req)
+  res.send("song sent").status(200);
 })
 
 app.listen(port, () => {
