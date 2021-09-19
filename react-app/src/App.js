2a8f7b864b3ae591054858607610faf42855f75c
sarah23398 <sarahali@cmail.carleton.ca>
2021-09-19 03:21:10 -0400

    Added route for homepage

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
 import React from 'react'
 import './App.scss';
+import Home from './Home';
 import axios from 'axios';
-
-let fileChange = ()=>{
-  let fileName = document.getElementById("filetoupload").value;
-  console.log(fileName)
-  if(fileName)
-    document.getElementById("uploadFile").innerHtml = `<p>${fileName}</p>`
-  else{
-    document.getElementById("uploadFile").innerHtml = `<p>No file chosen</p>`
-  }
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [data, setData] = React.useState("");
@@ -27,27 +23,15 @@ function App() {
   return (
     <><link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Arvo" /><link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'></link>
     <div className="App">
-      <header className="App-header">
-      </header>
-      <div className="background-splash">
-        <img className="background-img" src="/croppedallthree.png" />
-        <div className="main-content stack-top">
-        <h1 className="page-title">Play your favorite songs on your favorite instruments.</h1>
-        <div className="file-upload-container">
-        <div className="upload-button">
-          <p>
-            Upload MP3 or WAV
-          </p>
-        </div>
-        <input type="file" id="filetoupload" name="filetoupload" accept="audio/mp3, audio/wav" onChange={fileChange} />
-        <div className="upload-status" id="uploadFile">
-            <p>No file chosen</p>
-          </div>
-        </div>
-      </div>
-      </div>
-  
-    </div></>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+    </>
   );
 }
  
